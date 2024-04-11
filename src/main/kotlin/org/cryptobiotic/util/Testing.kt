d9d3be09ed62dabc448eb08f7d2490f0ad58bbbf
JohnLCaron <jcaron1129@gmail.com>
2024-04-11 12:24:34 -0600

    Move Testing class to main.

--- src/main/kotlin/org/cryptobiotic/util/Testing.kt
+++ src/main/kotlin/org/cryptobiotic/util/Testing.kt
@@ -1,8 +1,5 @@
 package org.cryptobiotic.util
 
-//val testOut = "/home/stormy/tmp/testOut/egkec"
-//val testOutMixnet = "/home/stormy/tmp/testOut/egmixnet"
-
 class Testing {
     companion object {
         val tmpdir = java.nio.file.Files.createTempDirectory("testOut").toFile().absolutePath
